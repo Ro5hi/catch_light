31eba46987de80143f48bb7d0acf0cd171fb1021
Shirlen Detablan <slen.detablan@gmail.com>
2020-08-14 23:25:29 -0400

    fixed shotgun error failed to load

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
       @user = User.all
       erb :'photos/recent'
     end 
-    binding.pry  
+
     get '/photos/:id' do 
       @photo.id = Photo.find_by(params[:user_id])
       erb :'photos/recent/#{@photo.id}'
@@ -16,14 +16,11 @@ class PhotosController < ApplicationController
         @photo = Photo.create!(url:@file)
         current_user.photos << @photo
         redirect 'photos/recent'
-      end 
     end
-
+    
     post '/photos/:id' do 
       @photo = Photo.find_by(params[:id])
       @photo.save 
       redirect to 'photos/recent/#{@photo.id}'
     end
-
- 
 end
