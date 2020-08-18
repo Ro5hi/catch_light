ace521c23d66bfe792247a4d3254c3e548f4530e
Shirlen Detablan <slen.detablan@gmail.com>
2020-08-18 19:14:14 -0400

    testing delete photo, err on photos/delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,39 +11,39 @@ class PhotosController < ApplicationController
     end 
 
     post '/photos' do
-        puts "#{params[:file]}"
-        photo = Photo.new
+      puts "#{params[:file]}"
+      photo = Photo.new
+      user = User.new 
 
-        photo.url = params[:file][:filename]
-        photo.save!
-      
-        File.open("./public/uploads/#{photo.url}", 'wb') do |f|
-        f.write(params[:file][:tempfile].read)
+      photo.url = params[:file][:filename]
+      photo.save!
+        
+      File.open("./public/uploads/#{photo.url}", 'wb') do |f|
+      f.write(params[:file][:tempfile].read)
       end
       redirect to("/photos/recent")
-    end
+    end    
 
-    get '/photos/:id' do 
-      photo = Photo.new 
-      photo = Photo.find_by(id: params[:id])
-      redirect to("/photos")
-    end
+    # get '/photos/:id' do 
+    #   photo = Photo.new 
+    #   photo = Photo.find_by(id: params[:id])
+    #   redirect to("/photos")
+    # end
     
-    post '/photos/:id' do 
-      @photo = Photo.find_by(params[:user_id])
-      @photo.name = params['photo']['name']
-      @photo.save 
-      redirect to "photos/#{photo.id}"
-    end
+    # post '/photos/:id' do 
+    #   @photo = Photo.find_by(params[:user_id])
+    #   @photo.name = params[:file][:filename]
+    #   @photo.save 
+    #   redirect to "photos/#{photo.id}"
+    # end
 
     get '/photos/delete' do 
       if logged_in?
         @photo = Photo.find_by(params[:user_id])
-        if @photo.delete 
+        @photo.delete 
           erb :'user/home'
         else 
           redirect to '/photos'
-        end 
       end 
     end
 
