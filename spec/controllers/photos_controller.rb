270c141ea7b55c767dd541c4ebf9adf51f28c655
Shirlen Detablan <slen.detablan@gmail.com>
2020-08-19 13:02:26 -0400

    spec file for controllers & spec.md

--- spec/controllers/photos_controller.rb
+++ spec/controllers/photos_controller.rb
@@ -1,30 +1,29 @@
-# require_relative "spec_helper"
+require_relative "spec_helper"
 
-# def app 
-#     PhotosController
-# end 
+def app 
+    PhotosController
+end 
 
-#     describe "GET '/photos'" do 
-#     it "responds with a 200 status code'" do
-#         visit '/recent'
-#     end 
+    describe "GET '/photos'" do 
+        it "responds with a 200 status code'" do
+        visit '/recent'
+    end 
 
-#     describe "POST '/photos'" do 
-#     it 'allows the user to upload photos'
-#         get '/photos/new'
-#     end
+    describe "POST '/photos/upload'" do 
+        it 'allows the user to upload photos'
+        get '/photos/new'
+    end
 
-#     describe "GET '/photos/:id'" do 
-#     it 'lets the user obtain the photo id' do 
-#         get '/photos'
-#         visit '/photos/:id'
-#         expect(photo.user_id)to_eq(user.id)
-#     end 
+    describe "GET '/photos/:id'" do 
+        it 'obtains the photo id' do 
+        get '/photos'
+        visit '/photos/:id'
+        expect(photo.user_id)to_eq(user.id)
+    end 
 
-#     describe "POST '/photos/:id'" do 
-#         it 'redirects user to photo' do 
-#             get '/photos'
-#             visit '/recent/#{photo.id}'
-#     end 
+    describe "POST '/photos/:id'" do 
+        it 'redirects user to photo' do 
+        get '/photos/:id'
+    end 
 
-# end 
+end 
--- spec/controllers/users_controller.rb
+++ spec/controllers/users_controller.rb
@@ -1,65 +1,79 @@
-# require_relative "spec_helper"
+require_relative "spec_helper"
 
-# def app
-#     UsersController
-# end
+def app
+    UsersController
+end
 
-# describe "GET '/register'" do 
-#     it 'returns a 200 status code' do 
-#       get '/register'
-#       expect(last_response.status).to eq(200)
-#     end
+describe UsersController do
+    it "responds with the user profile and a 200 status code" do
+    get '/user'
+    session[:user_id] = 1
 
-#     it 'returns contains a form to register' do 
-#       visit '/register'
-#       expect(page).to have_selector("form")
-#       expect(page).to have_field(:password)
-#       expect(page).to have_field(:email)
-#     end
-#   end
+    expect(last_response.body).to include('index')
+    @user = User.create(username: "username", email: "user@email.com", password: "password")
+end
 
-#   describe "POST '/signup'" do 
-#     it 'returns a 200 status code' do 
-#       visit '/register'
-#       fill_in "password", :with => "testpassword1"
-#       fill_in "email", :with => "test@test.com"
+it 'has a secure password' do
+    expect(subject.authenticate('oops')).to eq(false)
+    expect(subject.authenticate('farmer')).to eq(subject)
+end
+
+describe "GET '/register'" do 
+    it 'returns a 200 status code' do 
+      get '/register'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'returns contains a form to register' do 
+      visit '/register'
+      expect(page).to have_selector("form")
+      expect(page).to have_field(:password)
+      expect(page).to have_field(:email)
+    end
+  end
+
+  describe "POST '/signup'" do 
+    it 'returns a 200 status code' do 
+      visit '/register'
+      fill_in "password", :with => "testpassword1"
+      fill_in "email", :with => "test@test.com"
       
-#       click_button "Submit"
-#       expect(page.current_path).to eq('users/home')
-#       expect(page.status_code).to eq(200)
-#     end
-#   end
+      click_button "Submit"
+      expect(page.current_path).to eq('users/home')
+      expect(page.status_code).to eq(200)
+    end
+  end
 
-#   describe "GET '/login'" do
-#     it 'returns a 200 status code' do
-#       get '/login'
-#       expect(last_response.status).to eq(200)
-#     end
+  describe "GET '/login'" do
+    it 'returns a 200 status code' do
+      get '/login'
+      expect(last_response.status).to eq(200)
+    end
 
-#     it 'returns a form to login' do
-#       visit '/login'
-#         expect(page).to have_field(:email)
-#         expect(page).to have_field(:email)
-#     end
-#   end
+    it 'returns a form to login' do
+      visit '/login'
+        expect(page).to have_field(:email)
+        expect(page).to have_field(:email)
+    end
+  end
 
-#   describe "POST '/login'" do
-#     it 'returns a 200 status code' do
-#       visit '/login'
-#       fill_in "email", :with => "email"
-#       fill_in "password", :with => "password"
-#       click_button "Login"
-#       expect(page.current_path).to eq('/index')
-#       expect(page.status_code).to eq(200)
-#     end
+  describe "POST '/login'" do
+    it 'returns a 200 status code' do
+      visit '/login'
+      fill_in "email", :with => "email"
+      fill_in "password", :with => "password"
+      click_button "Login"
+      expect(page.current_path).to eq('/index')
+      expect(page.status_code).to eq(200)
+    end
 
-#     it 'gets to post' do
-#       params = {
-#       "email"=> "email", "password" => "password"
-#       } 
-#       post '/login', params
-#         expect(last_response.status).to eq(302)
-#     end
+    it 'gets to post' do
+      params = {
+      "email"=> "email", "password" => "password"
+      } 
+      post '/login', params
+        expect(last_response.status).to eq(302)
+    end
 
-#   end
-# end
+  end
+end
